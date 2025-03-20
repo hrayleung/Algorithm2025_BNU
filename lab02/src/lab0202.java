7e17efc7716455f876924bb66c1b9d267d19888c
hrayleung <hinrayleung@gmail.com>
2025-03-20 20:39:18 +0800

    lab0203

--- lab02/src/lab0202.java
+++ lab02/src/lab0202.java
@@ -88,41 +88,23 @@ public class lab0202 {
         splitMatrix(a, a11, a12, a21, a22, newSize);
         splitMatrix(b, b11, b12, b21, b22, newSize);
 
-        int[][] m1 = strassenMultiply(addMatrix(a11, a22, newSize),
-                addMatrix(b11, b22, newSize), newSize);
-        int[][] m2 = strassenMultiply(addMatrix(a21, a22, newSize),
-                b11, newSize);
-        int[][] m3 = strassenMultiply(a11,
-                subtractMatrix(b12, b22, newSize), newSize);
-        int[][] m4 = strassenMultiply(a22,
-                subtractMatrix(b21, b11, newSize), newSize);
-        int[][] m5 = strassenMultiply(addMatrix(a11, a12, newSize),
-                b22, newSize);
-        int[][] m6 = strassenMultiply(subtractMatrix(a21, a11, newSize),
-                addMatrix(b11, b12, newSize), newSize);
-        int[][] m7 = strassenMultiply(subtractMatrix(a12, a22, newSize),
-                addMatrix(b21, b22, newSize), newSize);
+        int[][] m1 = strassenMultiply(addMatrix(a11, a22, newSize), addMatrix(b11, b22, newSize), newSize);
+        int[][] m2 = strassenMultiply(addMatrix(a21, a22, newSize), b11, newSize);
+        int[][] m3 = strassenMultiply(a11, subtractMatrix(b12, b22, newSize), newSize);
+        int[][] m4 = strassenMultiply(a22, subtractMatrix(b21, b11, newSize), newSize);
+        int[][] m5 = strassenMultiply(addMatrix(a11, a12, newSize), b22, newSize);
+        int[][] m6 = strassenMultiply(subtractMatrix(a21, a11, newSize), addMatrix(b11, b12, newSize), newSize);
+        int[][] m7 = strassenMultiply(subtractMatrix(a12, a22, newSize), addMatrix(b21, b22, newSize), newSize);
 
-        int[][] res11 = addMatrix(
-                subtractMatrix(
-                        addMatrix(m1, m4, newSize),
-                        m5, newSize),
-                m7, newSize);
+        int[][] res11 = addMatrix(subtractMatrix(addMatrix(m1, m4, newSize), m5, newSize), m7, newSize);
         int[][] res12 = addMatrix(m3, m5, newSize);
         int[][] res21 = addMatrix(m2, m4, newSize);
-        int[][] res22 = addMatrix(
-                subtractMatrix(
-                        addMatrix(m1, m3, newSize),
-                        m2, newSize),
-                m6, newSize);
-
+        int[][] res22 = addMatrix(subtractMatrix(addMatrix(m1, m3, newSize), m2, newSize), m6, newSize);
         joinMatrix(res, res11, res12, res21, res22, newSize);
         return res;
     }
 
-    private static void splitMatrix(int[][] original,
-                                    int[][] a11, int[][] a12,
-                                    int[][] a21, int[][] a22, int size) {
+    private static void splitMatrix(int[][] original, int[][] a11, int[][] a12, int[][] a21, int[][] a22, int size) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 a11[i][j] = original[i][j];
@@ -133,9 +115,7 @@ public class lab0202 {
         }
     }
 
-    private static void joinMatrix(int[][] res,
-                                   int[][] a11, int[][] a12,
-                                   int[][] a21, int[][] a22, int size) {
+    private static void joinMatrix(int[][] res, int[][] a11, int[][] a12, int[][] a21, int[][] a22, int size) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 res[i][j] = a11[i][j];
