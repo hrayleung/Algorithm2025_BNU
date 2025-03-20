5c54f595444bb2ee1b86bd87892a0e032315edc1
hrayleung <hinrayleung@gmail.com>
2025-03-20 13:01:29 +0800

    lab0203

--- lab02/src/lab0203.java
+++ lab02/src/lab0203.java
@@ -11,6 +11,42 @@ public class lab0203 {
             l[i] = sc.nextInt();
         }
 
+        System.out.println(quickSelect(l,0,l.length-1,k-1));
 
+    }
+
+    private static int quickSelect(int[] arr, int low, int high, int k) {
+        if (low == high) {
+            return arr[high];
+        }
+
+        int pivotIndex = partition(arr, low, high);
+
+        if (k == pivotIndex) {
+            return arr[k];
+        } else if (k < pivotIndex) {
+            return quickSelect(arr, low, pivotIndex - 1, k);
+        } else {
+            return quickSelect(arr, pivotIndex + 1, high, k);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, high);
+        return i;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
