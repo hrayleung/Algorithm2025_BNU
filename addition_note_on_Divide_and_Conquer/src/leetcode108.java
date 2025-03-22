7b8ef047d6d72741d8e00d45049999b89b4d93ed
hrayleung <hinrayleung@gmail.com>
2025-03-22 23:55:26 +0800

    leetcode108

---
+++ addition_note_on_Divide_and_Conquer/src/leetcode108.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    // Default constructor
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+
+public class leetcode108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = buildBST(nums, left, mid - 1);
+
+        root.right = buildBST(nums, mid + 1, right);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        TreeNode root = new leetcode108().sortedArrayToBST(nums);
+
+        printTree(root);
+
+    }
+
+    private static void printTree(TreeNode root) {
+        if (root == null) {
+            System.out.println("[]");
+            return;
+        }
+
+        List<String> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node == null) {
+                    result.add("null");
+                } else {
+                    result.add(String.valueOf(node.val));
+                    queue.offer(node.left);
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        // Remove trailing nulls
+        while (result.size() > 0 && result.get(result.size() - 1).equals("null")) {
+            result.remove(result.size() - 1);
+        }
+
+        // Print the tree
+        System.out.print("[");
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+            if (i < result.size() - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println("]");
+    }
+}
