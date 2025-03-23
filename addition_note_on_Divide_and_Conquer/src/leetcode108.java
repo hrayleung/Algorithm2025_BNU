b96955c11191b242867a6e4f640e3723b36c3a92
hrayleung <hinrayleung@gmail.com>
2025-03-23 23:39:58 +0800

    leetcode108

--- addition_note_on_Divide_and_Conquer/src/leetcode108.java
+++ addition_note_on_Divide_and_Conquer/src/leetcode108.java
@@ -1,26 +1,5 @@
 import java.util.*;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    // Default constructor
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-
-}
-
 public class leetcode108 {
     public TreeNode sortedArrayToBST(int[] nums) {
         return buildBST(nums, 0, nums.length - 1);
